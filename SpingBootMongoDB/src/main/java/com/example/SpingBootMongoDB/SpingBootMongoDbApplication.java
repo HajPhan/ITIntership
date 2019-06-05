6d0752798a9e5aa50967fb23c0a5d635b8a64b47
HajPhan <hajphan97@gmail.com>
2019-06-05 15:02:07 +0700

    first commit

---
+++ SpingBootMongoDB/src/main/java/com/example/SpingBootMongoDB/SpingBootMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpingBootMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootMongoDbApplication.class, args);
+	}
+
+}
